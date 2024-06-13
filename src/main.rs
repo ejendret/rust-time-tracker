ae88fe5bcf995b3968a23a0113d257b4b989e124
ejendret <ejendret@calpoly.edu>
2024-06-12 22:00:00 -0700

    Basic testing implemented

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::env;
 // use std::process::exit;
 use std::path::Path;
 use std::io::prelude::*;
+use std::fs;
 use std::fs::File;
 
 // Will create a hidden file containing project data in the current directory
-fn proj_create(title: String) {
-    // Get projedt path
+fn proj_create(title: String) -> bool {
+    // Get project path
     let proj_path = format!(".{}", title);
     let proj_path = Path::new(&proj_path);
 
@@ -21,6 +22,8 @@ fn proj_create(title: String) {
         Err(why) => panic!("couldn't write to {}: {}", proj_path.display(), why),
         Ok(()) => println!("successfully wrote {} to {}", title, proj_path.display())
     };
+
+    return true;
 }
 
 fn main() {
@@ -30,4 +33,28 @@ fn main() {
     let first = &args[1];
 
     proj_create(first.to_string());
+}
+
+// Testing here for now
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_proj_create() {
+        // Setup test file name and path
+        let file_title = "test".to_string();
+        let file_path = format!(".{}", file_title);
+        let file_path = Path::new(&file_path);
+
+        // Remove file if it already exists
+        if file_path.exists() {
+            fs::remove_file(file_path).expect("Failed to delete file prior to running tests.");
+        }
+
+        // Test creating the file
+        let result = proj_create("test".to_string());
+        assert!(result);
+        let result = std::panic::catch_unwind(||proj_create("test".to_string()));
+        assert!(result.is_err());
+    }
 }
