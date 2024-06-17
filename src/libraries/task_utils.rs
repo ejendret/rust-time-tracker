9bdb255e415ca9fcdf4e091427d31529abb3953d
ejendret <ejendret@calpoly.edu>
2024-06-16 20:37:38 -0700

    Simplified task structure

--- src/libraries/task_utils.rs
+++ src/libraries/task_utils.rs
@@ -1,35 +1,58 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
-extern crate chrono;
-use chrono::{DateTime, Duration, Utc};
 
-struct Session {
-    start_time: DateTime<Utc>,
-    end_time: Option<DateTime<Utc>>,
-    duration: Option<Duration>,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Session {
+    pub start_time: DateTime<Utc>,
+    pub end_time: DateTime<Utc>,
+    duration: i64
 }
 
 impl Session {
-    fn new(start: DateTime<Utc>) -> Session {
+    pub fn new(start: DateTime<Utc>) -> Session {
         Session {
             start_time: start,
-            end_time: None,
-            duration: None,
-        }
-    }
-
-    // Method to set the end time and calculate duration
-    fn end_session(&mut self, end: DateTime<Utc>) -> Result<(), String> {
-        if self.end_time.is_none() {
-            self.end_time = Some(end);
-            self.duration = Some(end.signed_duration_since(self.start_time));
-            Ok(())
-        } else {
-            Err("End time has already been set".to_string())
+            end_time: start,
+            duration: 0,
         }
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-struct Task {
-    title: String,
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Task {
+    pub title: String,
+    pub sessions: Vec<Session>,
+    pub total_duration: i64,
+}
+
+impl Task {
+    pub fn new(title: String) -> Task {
+       Task {
+        title: title,
+        sessions: Vec::new(),
+        total_duration: 0,
+       }
+    }
+
+    pub fn start_session(&mut self) {
+        // Proceed only if there are no sessions or the last session is complete
+        let length = self.sessions.len();
+        if length == 0 || self.sessions[length - 1].duration != 0 {
+            let new_session = Session::new(Utc::now());
+            self.sessions.push(new_session);
+        } else {
+            println!("Cannot start a session while another is ongoing.");
+        }
+    }
+
+    pub fn end_session(&mut self, end: DateTime<Utc>) {
+        let length = self.sessions.len();
+        if length > 0 && self.sessions[length - 1].duration == 0 {
+            self.sessions[length - 1].end_time = end;
+            self.sessions[length - 1].duration = (end - self.sessions[length - 1].start_time).num_seconds();
+        } else {
+            println!("No ongoing session to end.");
+        }
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,10 @@
 // My libraries
 // use rust_time_tracker::project_utils;
 use rust_time_tracker::config_utils;
-use rust_time_tracker::task_utils;
+use rust_time_tracker::task_utils::Task;
 // Outside libraries
 // use std::env;
 
 fn main() {
-    // Retrieve arguments
-    // let args: Vec<String> = env::args().collect();
 
-    // let first = &args[1];
-
-    // if let Err(e) = project_utils::proj_create(first) {
-    //     println!("[main] error creating project: {}", e);
-    // }
-
-    // let _ = match config_utils::check_config() {
-    //     Ok(current_proj) => println!("Current project is: {}", current_proj),
-    //     Err(why) => println!("Failed with error: {}", why),
-    // };
-
-    // let _ = match config_utils::change_config("donkey kong".to_string()) {
-    //     Ok(()) => println!("Success"),
-    //     Err(why) => println!("Curses and damnation upon ye {}", why),
-    // };
 }
