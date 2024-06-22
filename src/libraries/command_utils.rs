4dd10557c8c3bd14ff92a4b0e2563d8b8b3e49e3
ejendret <ejendret@calpoly.edu>
2024-06-21 20:17:51 -0700

    process_args will return an enum for easier testing

--- src/libraries/command_utils.rs
+++ src/libraries/command_utils.rs
@@ -1,10 +1,22 @@
-pub fn process_args(args: &Vec<String>) {
-    println!("Num args: {}", args.len());
+#[derive(PartialEq, Eq)]
+pub enum Command {
+    Invalid,
+    Help,
+    ProjecCreate,
+    ProjctDelete,
+    ProjectView,
+}
+
+pub fn process_args(args: &Vec<String>) -> Command {
     match args.len() {
         // One argument
-        2 => {}
-        _ => {
-            println!("Please enter a valid command\nhint: tracker help");
+        2 => {
+            match args[1].as_str() {
+                "help" => Command::Help,
+                _ => Command::Invalid
+            }
         }
-    }
+        // Two arguments
+        _ => Command::Invalid
+        }
 }
--- tests/command_tests.rs
+++ tests/command_tests.rs
@@ -1,4 +1,4 @@
-use rust_time_tracker::command_utils::process_args;
+use rust_time_tracker::command_utils::{process_args, Command};
 
 #[test]
 fn test_process() {
@@ -7,5 +7,12 @@ fn test_process() {
         .map(String::from)
         .collect();
 
-    process_args(&args);
+    assert!(process_args(&args) == Command::Invalid);
+
+    let args: Vec<String> = vec!["tracker", "help"]
+    .into_iter()
+    .map(String::from)
+    .collect();
+
+    assert!(process_args(&args) == Command::Help);
 }
